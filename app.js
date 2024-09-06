81547ab36dd8409f9d9280b6e984f85396535b5f
gusyaaa <askerzadehuseyn2005@gmail.com>
2024-09-06 17:57:02 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+let h1 = document.querySelector("h1")
+h1.innerText = "Series"
+
+let li3 = document.querySelector("li:nth-child(3)")
+let li6 = document.querySelector("li:last-child")
+
+
+
+
+let ul = document.querySelector("ul")
+let primary = document.querySelector(".primary")
+
+li6.style.backgroundColor = "teal";
+li6.style.width = "200px";
+li3.style.width = "200px";
+li3.style.backgroundColor = "teal";
